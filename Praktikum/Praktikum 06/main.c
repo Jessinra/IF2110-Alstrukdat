21def5c75827ff40d0520daf33cc33506c432e08
Jessin ra <jessinra@gmail.com>
2017-10-15 22:22:15 +0700

    Praktikum 06 & 07

---
+++ Praktikum/Praktikum 06/main.c
@@ -0,0 +1,62 @@
+
+#include "prioqueue.h"
+#include <stdio.h>
+
+
+int main (void){
+
+	Queue Antri;
+	infotype element;
+
+	CreateEmpty(&Antri,5);
+	PrintQueue(Antri);
+
+	element.prio = 3;
+	element.info = 5;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+	element.prio = 6;
+	element.info = 2;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+	element.prio = 2;
+	element.info = 1;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+	element.prio = 1;
+	element.info = 5;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+	element.prio = 2;
+	element.info = 5;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+	Del(&Antri, &element);
+    PrintQueue(Antri);
+    Del(&Antri, &element);
+    PrintQueue(Antri);
+    Del(&Antri, &element);
+    PrintQueue(Antri);
+    Del(&Antri, &element);
+    PrintQueue(Antri);
+
+
+	element.prio = 4;
+	element.info = 5;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+
+
+    element.prio = 1;
+	element.info = 55;
+	Add(&Antri, element);
+	PrintQueue(Antri);
+
+return 0;
+}
