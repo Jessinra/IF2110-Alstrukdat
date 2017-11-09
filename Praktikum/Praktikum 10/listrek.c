e40903cd612c5c0bde7ef5b778c8ad39bee44689
Jessin ra <jessinra@gmail.com>
2017-11-09 11:56:17 +0700

    Prak 10

--- Praktikum/Praktikum 10/listrek.c
+++ Praktikum/Praktikum 10/listrek.c
@@ -232,3 +232,208 @@ boolean Search (List L, infotype X)
     }
 }
 
+
+/*** Pencarian nilai ekstrim ***/
+/* Prekondisi untuk Max/Min/Sum/Average : List tidak kosong */
+infotype MaxList (List L)
+/* Mengirimkan nilai info(P) yang maksimum */
+{
+	infotype max;
+	
+	if (IsEmpty(L)){
+		return -9999;
+	}
+	else{
+		
+		max = MaxList(Tail(L));
+		if (FirstElmt(L) > max){
+			return FirstElmt(L);
+		}
+		else{
+			return max;
+		}
+		
+	}
+}
+
+infotype MinList (List L)
+/* Mengirimkan nilai info(P) yang minimum */
+{
+	infotype min;
+	
+	if (IsEmpty(L)){
+		return 9999;
+	}
+	else{
+		
+		min = MinList(Tail(L));
+		if (FirstElmt(L) < min){
+			return FirstElmt(L);
+		}
+		else{
+			return min;
+		}
+		
+	}
+}
+
+infotype SumList (List L)
+/* Mengirimkan total jumlah elemen List L */
+{
+	
+	if (IsEmpty(L)){
+		return 0;
+	}
+	else{
+		return FirstElmt(L) + SumList(Tail(L));
+	}
+}
+
+float AverageList (List L)
+/* Mengirimkan nilai rata-rata elemen list L */
+{
+	return (float) SumList(L)/NbElmtList(L);
+}
+
+
+/*** Operasi-Operasi Lain ***/
+List InverseList (List L)
+/* Mengirimkan list baru, hasil invers dari L dengan menyalin semua elemen list.
+Semua elemen list baru harus dialokasi */
+/* Jika alokasi gagal, hasilnya list kosong */
+{
+	if (IsEmpty(L)){
+		return Nil;
+	}
+	else{
+		return KonsB(InverseList(Tail(L)),FirstElmt(L));
+	}
+	
+}
+
+void SplitPosNeg (List L, List *L1, List *L2)
+/* I.S. L mungkin kosong */
+/* F.S. Berdasarkan L, dibentuk dua buah list L1 dan L2 */ 
+/* L1 berisi semua elemen L yang positif atau 0, sedangkan L2 berisi
+semua elemen L yang negatif; semua dengan urutan yang sama seperti di L */
+/* L tidak berubah: Semua elemen L1 dan L2 harus dialokasi */
+/* Jika L kosong, maka L1 dan L2 kosong */
+{
+	List T1, T2;
+	
+	if (IsEmpty(L)){
+		*L1 = Nil;
+		*L2 = Nil;
+	}
+	else{
+		
+		SplitPosNeg(Tail(L), &T1, &T2);
+		
+		if (FirstElmt(L) >= 0 ){
+			
+			*L1 = Konso(FirstElmt(L),T1);
+			*L2 = T2;
+		}
+		else {
+			
+			*L1 = T1;
+			*L2 = Konso(FirstElmt(L),T2);
+		}	
+	}
+}
+
+
+
+void SplitOnX (List L, infotype X, List *L1, List *L2)
+/* I.S. L dan X terdefinisi, L1 dan L2 sembarang. */
+/* F.S. L1 berisi semua elemen L yang lebih kecil dari X, dengan urutan
+kemunculan yang sama, L2 berisi semua elemen L yang tidak masuk ke
+L1, dengan urutan kemunculan yang sama. */
+{
+	List T1, T2;
+	
+	if (IsEmpty(L)){
+		*L1 = Nil;
+		*L2 = Nil;
+	}
+	else{
+		
+		SplitOnX(Tail(L), X, &T1, &T2);
+		
+		if (FirstElmt(L) < X ){
+			
+			*L1 = Konso(FirstElmt(L),T1);
+			*L2 = T2;
+		}
+		else {
+			
+			*L1 = T1;
+			*L2 = Konso(FirstElmt(L),T2);
+		}	
+	}
+}
+
+int ListCompare (List L1, List L2)
+/* Menghasilkan: -1 jika L1 < L2, 0 jika L1 = L2, dan 1 jika L1 > L2 */
+/* Jika L[i] adalah elemen L pada urutan ke-i dan |L| adalah panjang L: */
+/* L1 = L2: |L1| = |L2| dan untuk semua i, L1[i] = L2[i] */
+/* L1 < L2: Jika i adalah urutan elemen yang berbeda yang terkecil
+dari L1 dan L2, L1[i]<L2[i] atau: Jika pada semua elemen pada
+urutan i yang sama, L1[i]=L2[i], namun |L1|<|L2| */
+/* Contoh: [3,5,6,7] < [4,4,5,6]; [1,2,3]<[1,2,3,4] */
+/* L1>L2: kebalikan dari L1<L2 */
+{
+	if (IsEmpty(L1) && IsEmpty(L2)){
+		return 0;
+	}
+	
+	else if (IsEmpty(L1) && !IsEmpty(L2)){
+		return -1 ;
+	}
+	
+	else if (!IsEmpty(L1) && IsEmpty(L2)){
+		return 1 ;
+	}
+	
+	else{
+		
+		if (FirstElmt(L1) < FirstElmt(L2)){
+			return -1;
+		}
+		
+		else if (FirstElmt(L1) > FirstElmt(L2)){
+			return 1;
+		}
+		
+		/* Sama */ 
+		else {
+			return ListCompare(Tail(L1), Tail(L2));
+		}
+	}
+}
+
+boolean IsAllExist (List L1, List L2)
+/* Menghasilkan true jika semua elemen dalam L1 terdapat dalam L2 (tanpa
+memperhatikan urutan ataupun banyaknya elemen).
+Kedua list mungkin kosong. Jika L1 kosong, maka hasilnya false. */
+{
+	boolean isallexist;
+	
+	/* Kasus khusus */
+	if(IsEmpty(L1)){
+		
+		return false;
+	}
+	
+	else if (IsOneElmt(L1)){
+		return Search(L2, FirstElmt(L1));
+	}
+	
+	else{
+		printf("ye");
+		isallexist = IsAllExist(Tail(L1),L2);
+		return (Search(L2, FirstElmt(L1))) && isallexist;
+	}
+	
+
+}
